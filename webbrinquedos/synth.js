c3f037c90c5fcba4f8c8d19dc064cebb61ea81e9
Bruno <brunomanarin@icloud.com>
2019-01-08 20:27:18 -0200

    sunjpeg

--- webbrinquedos/synth.js
+++ webbrinquedos/synth.js
@@ -2,7 +2,7 @@ var image;
 
 
 function preload(){
- image = loadImage('https://raw.githubusercontent.com/brunomanarin/brunomanarin.github.io/master/media/eu2.jpeg');
+ image = loadImage('https://raw.githubusercontent.com/brunomanarin/brunomanarin.github.io/master/media/synthsun.jpg');
 }
 
 
@@ -11,27 +11,26 @@ function setup(){
 	rectMode(CENTER);
 }
 function draw(){
-	background(214, 0, 214);
+	background(89, 0, 89);
 	camera(0, 0, sin(frameCount * 0.01) * 100, 0, 0, 0, 0, 1, 0);
 
 
 	push();
 	rotateX(5);
-	translate(-width/2,-height/2,sin(frameCount * 0.01)*10);
+	translate(-width/2,-height/2, 10);
 	fill(89, 0, 89);
 	for(let x = -500; x<width+1000; x+=100){
 		for(let y = 0 ; y<height+1000; y+=100){
-			rect(x,y,90,90);
+			rect(x,y,98,90);
 		}
 	}
 	pop();
 
 	push();
-	translate(0,-height,-1000+sin(frameCount * 0.01)*100);
-	scale(3);
-	normalMaterial();
-	texture(image);
-	plane(width,height/2);
+	rotateX(5);
+	translate(0,0, -10);
+	fill(214, 0, 214);
+	rect(0,0,width+1000,height+400);
 	pop();
 
 }
