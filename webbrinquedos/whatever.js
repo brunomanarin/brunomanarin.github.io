4d637413262e289b0d097ac4a3cb974aa4521261
Bruno <brunomanarin@icloud.com>
2019-01-08 17:05:16 -0200

    testing 3d models in webgl

--- webbrinquedos/whatever.js
+++ webbrinquedos/whatever.js
@@ -1,7 +1,16 @@
+var venus;
 
+
+function preload() {
+  venus = loadModel('https://raw.githubusercontent.com/brunomanarin/brunomanarin.github.io/master/media/Glock%203d.obj');
+}
 function setup(){
 	createCanvas(400,400,WEBGL);
 }
 function draw(){
 	background(170);
+	rotateX(mouseX*0.1);
+	rotateY(mouseY*0.1);
+	scale(40);
+	model(venus)
 }
