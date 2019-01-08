1386cecb71dac92a96629e43fd422063ce448398
Bruno <brunomanarin@icloud.com>
2019-01-08 13:21:43 -0200

    testing 3d models in webgl

--- webbrinquedos/heyo.js
+++ webbrinquedos/heyo.js
@@ -1,13 +1,53 @@
-function setup(){
-	createCanvas(windowWidth,windowHeight);
-	rectMode(CENTER);
+// feito por Dan Shiffman-
+var cols, rows;
+var scl = 100;
+var w = 4400;
+var h = 6000;
+
+var flying = 0;
+
+var terrain = [];
+var canvas;
+function setup() {
+  canvas = createCanvas(windowWidth, windowHeight, WEBGL);
+  	canvas.position(0,100);
+  	canvas.style('z-index','-1');
+  cols = w / scl;
+  rows = h/ scl;
+
+  for (var x = 0; x < cols; x++) {
+    terrain[x] = [];
+    for (var y = 0; y < rows; y++) {
+      terrain[x][y] = 0; //specify a default value for now
+    }
+  }
 }
-function draw(){
-	rect(width/2, height-100,100,500)
-	fill(119, 45, 0);
-	noStroke();
-	push();
-	rect(width/2,height-400,30,100);
-	rotate(30);
-	pop();
+
+function draw() {
+
+  flying -= 0.06;
+  var yoff = flying;
+  for (var y = 0; y < rows; y++) {
+    var xoff = 0;
+    for (var x = 0; x < cols; x++) {
+      terrain[x][y] = map(noise(xoff, yoff), 0, 1, -250, 250);
+      xoff += 0.2;
+    }
+    yoff += 0.2;
+  }
+
+
+  background(0,0,170);
+  translate(0, 50);
+  rotateX(PI/3);
+  fill(200,200,200, 50);
+  translate(-w/2, -h/2);
+  for (var y = 0; y < rows-1; y++) {
+    beginShape(TRIANGLE_STRIP);
+    for (var x = 0; x < cols; x++) {
+      vertex(x*scl, y*scl, terrain[x][y]);
+      vertex(x*scl, (y+1)*scl, terrain[x][y+1]);
+    }
+    endShape();
+  }
 }
