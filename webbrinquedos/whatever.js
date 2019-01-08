16a14595145faacdf423af9e8592e5c80db48e72
Bruno <brunomanarin@icloud.com>
2019-01-08 16:54:50 -0200

    testing 3d models in webgl

--- webbrinquedos/whatever.js
+++ webbrinquedos/whatever.js
@@ -1,15 +1,7 @@
-var venus;
 
-
-function preload() {
-  venus = loadModel('../media/venus.obj');
-}
 function setup(){
 	createCanvas(400,400,WEBGL);
 }
 function draw(){
 	background(170);
-	rotateX(mouseX*0.1);
-	rotateY(mouseY*0.1);
-	model(venus);
 }
