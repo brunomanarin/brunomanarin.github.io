21b0597f7f3219804d8efc84768b149e7ece3474
Bruno <brunomanarin@icloud.com>
2019-01-08 17:58:05 -0200

    testing 3d models in webgl

--- webbrinquedos/whatever.js
+++ webbrinquedos/whatever.js
@@ -1,18 +1,37 @@
 var venus;
+var img;
+var cam;
 var a = 0.5;
 
 function preload() {
   venus = loadModel('https://raw.githubusercontent.com/brunomanarin/brunomanarin.github.io/master/media/venuslowpoly.obj');
+  img = loadImage('https://raw.githubusercontent.com/brunomanarin/brunomanarin.github.io/master/media/xpfundo.jpg');
 }
 function setup(){
 	createCanvas(400,400,WEBGL);
 }
 function draw(){
-	translate(0,100);
-	background(170);
+	push();
+	translate(-150,150);
 	rotateX(5);
 	rotateZ(a);
-	model(venus)
+	normalMaterial();
+	scale(0.8);
+	model(venus);
+	pop();
+
+	push();
+	translate(150,150);
+	rotateX(5);
+	rotateZ(a);
+	normalMaterial();
+	scale(0.8);
+	model(venus);
+	pop();
 	a += 0.07;
-	console.log(a);
+
+	push();
+	texture(img);
+	plane(400,400);
+	pop();
 }
