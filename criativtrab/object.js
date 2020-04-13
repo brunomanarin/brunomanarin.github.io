aa06b8b1ad6896d012e13624d9c82af10f547988
bruno <bruno@aquare.la>
2020-04-13 18:45:09 -0300

    [WIP] Update files on repository

--- criativtrab/object.js
+++
@@ -1,9 +0,0 @@
-function SceneObject(x,y,w,h){
-    this.x= x
-    this.y= y
-    this.width = w
-    this.height = h
-    this.render = () =>{
-        rect(this.x,this.y,this.width,this.height)
-    }
-}
